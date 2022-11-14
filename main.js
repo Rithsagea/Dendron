93af74f59ebed81837aeb2400149bcf3d014e8a0
Rithsagea Aquadom <rithsagea@gmail.com>
2022-11-14 09:19:30 -0500

    playing around with queries

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const { Client } = require('@notionhq/client');
+const config = require('config');
+
+const notion = new Client({
+	auth: config.get('notion.key')
+});
+
+// (async () => {
+//   const response = await notion.databases.retrieve({ database_id: config.get('notion.db.notes') });
+//   console.log(JSON.stringify(response, null, 2));
+// })();
+
+(async () => {
+	const response = await notion.databases.query({
+		database_id: config.get('notion.db.notes')
+	});
+	console.log(JSON.stringify(response, null, 2));
+})();
